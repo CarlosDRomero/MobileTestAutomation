61f4751fa0a55a6e003d38bcf6c6d0df43828f9f
CarlosDRomero <cdavid.romeror@gmail.com>
2025-09-11 08:16:19 -0500

    set more capabilities and teardown method

--- src/test/java/com/mobile_testing/utils/BaseTest.java
+++ src/test/java/com/mobile_testing/utils/BaseTest.java
@@ -2,6 +2,7 @@ package com.mobile_testing.utils;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import java.net.MalformedURLException;
@@ -29,12 +30,23 @@ public class BaseTest {
     public AppiumDriver createDriver() {
         UiAutomator2Options options = new UiAutomator2Options()
                 .setPlatformName("android")
+                .setDeviceName("emulator-5554")
                 .setApp(PathGenerator.getApkPath())
-                .setDeviceName("emulator-5554");
+                .setAppActivity(".MainActivity")
+                .setAppPackage("com.wdiodemoapp")
+                ;
         try{
             return new AppiumDriver(new URL("http://127.0.0.1:4723/"),options);
         }catch(MalformedURLException e){
             throw new RuntimeException(e);
         }
+    }
+
+    /**
+     * Quits the driver after each test
+     */
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
     }
 }
