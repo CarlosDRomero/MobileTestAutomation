a3fb97561df2295fddbff320084fdd01a6e06de4
CarlosDRomero <cdavid.romeror@gmail.com>
2025-09-15 10:23:34 -0500

    add documentation and refactor gesture utils

--- src/test/java/com/mobile_testing/utils/gesture/GestureController.java
+++ src/test/java/com/mobile_testing/utils/gesture/GestureController.java
@@ -15,7 +15,14 @@ public class GestureController {
         screenWidth = driver.manage().window().getSize().getWidth();
         screenHeight = driver.manage().window().getSize().getHeight();
     }
-    public Vector getDirectionVector(SwipeDirection direction, int length) {
+
+    /**
+     * Creates a horizontal or a vertical vector with a certain value
+     * @param direction: An enum value that could be VERTICAL or HORIZONTAL
+     * @param length: The distance of the vector component used depending on the direction
+     * @return A vector with a 0 and {@code length} as X or Y value depending on the direction
+     */
+    public Vector getDirectionVector(SwipeDirection direction, float length) {
         switch (direction){
             case VERTICAL:
                 return new Vector(0,length);
@@ -24,9 +31,16 @@ public class GestureController {
         }
         return null;
     }
-    public Vector getDirectionVector(SwipeDirection direction, float percentageLength){
-        return getDirectionVector(direction,
-            (int)(percentageLength * (direction == SwipeDirection.HORIZONTAL? screenWidth : screenHeight)));
+
+    /**
+     * Creates a horizontal or a vertical vector with a certain value
+     * @param direction: An enum value that could be VERTICAL or HORIZONTAL
+     * @param percentageLength: The percentage distance of the vector component used depending on the direction
+     * @return A vector with a 0 and {@code percentageLength} (converted to pixels) as X or Y value depending on the direction
+     */
+    public Vector getPixelDirectionVector(SwipeDirection direction, float percentageLength){
+        Vector directionVector = getDirectionVector(direction,percentageLength);
+        return new Vector(directionVector.x * screenWidth,directionVector.y * screenHeight);
     }
     public Vector getElementCenter(WebElement element){
         int X = (element.getSize().width / 2) + element.getRect().getX();
@@ -35,8 +49,22 @@ public class GestureController {
         return  new Vector(X, Y);
     }
 
-    public Vector percentageToPixels(Vector vector) {
-        return new Vector(vector.x * screenWidth, vector.y * screenHeight);
+    /**
+     * Receives percentage X and Y values and converts to absolute (pixels) values, using screen real width and height
+     * @param xPercentage: The screen percentage in X axis
+     * @param yPercentage: The screen percentage in Y axis
+     * @return A vector with the pixel values equivalent to the percentages
+     */
+    public Vector getPixelsVector(float xPercentage, float yPercentage) {
+        return new Vector(xPercentage * screenWidth,yPercentage * screenHeight);
+    }
+     /**
+     * Receives percentage X and Y values and converts to absolute (pixels) values, using screen real width and height
+     * @param percentageVector: A vector containing the X and Y percentages values
+     * @return A vector with the pixel values equivalent to the percentages
+     */
+    public Vector getPixelsVector(Vector percentageVector) {
+        return getPixelsVector(percentageVector.x, percentageVector.y);
     }
 
 }
--- src/test/java/com/mobile_testing/utils/gesture/Vector.java
+++ src/test/java/com/mobile_testing/utils/gesture/Vector.java
@@ -19,12 +19,24 @@ public class Vector {
         this.y = y;
     }
 
+    /**
+     * Creates a new vector from the result of adding another vector to this
+     * @param vector: The vector to be added
+     * @return The add result vector
+     */
     public Vector add(Vector vector) {
         return  new Vector(x + vector.x, y + vector.y);
     }
+
+    /**
+     * Scales the vector by a certain value
+     * @param value: The value to multiply the vector components
+     * @return Returns the scaled vector result
+     */
     public Vector scale(float value) {
         return new Vector(x * value, y * value);
     }
+
     public int getIntX() {
         return (int)x;
     }
