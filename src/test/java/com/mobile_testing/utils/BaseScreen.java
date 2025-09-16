12b5b9bbdeabe9b55306e63e0bf2a4acf673929b
CarlosDRomero <cdavid.romeror@gmail.com>
2025-09-15 22:04:08 -0500

    add swipe from element to element method

--- src/test/java/com/mobile_testing/utils/BaseScreen.java
+++ src/test/java/com/mobile_testing/utils/BaseScreen.java
@@ -59,33 +59,47 @@ public abstract class BaseScreen {
 )
      */
     public void swipeFromElement(WebElement element, Vector directionVector) {
-        // Getting the start and end point of the swipe
+        // Getting the start point of the swipe
         Vector startPoint = gestureController.getElementCenter(element);
         swipeFromPoint(startPoint, directionVector);
 
     }
 
     /**
+     * Swipes from the center of an element to the center of the other
+     * @param startElement: Is the starting element of the swipe
+     * @param endElement: Is the element where the swipe ends
+     */
+    public void swipeFromElement(WebElement startElement, WebElement endElement) {
+        // Getting the start and end point of the swipe as the center of the elements
+        Vector startPoint = gestureController.getElementCenter(startElement);
+        Vector endPoint = gestureController.getElementCenter(endElement);
+        swipe(startPoint, endPoint, Duration.ofMillis(100));
+
+    }
+    /**
      * Swipes a certain distance from a starting point (Internally using the {@code swipe} method)
      * @param startPoint: The vector representing the starting point of the swipe, this must be given in pixels (use {@code gestureController} to convert values)
      * @param directionVector: The vector that sets the direction of the swipe from that center, this must be given in pixels (use {@code gestureController} to convert values)
      */
     public void swipeFromPoint(Vector startPoint, Vector directionVector) {
+        // Getting the start and end point of the swipe
+
         Vector endPoint = startPoint.add(directionVector);
-        swipe(startPoint, endPoint);
+        swipe(startPoint, endPoint, Duration.ofMillis(100));
     }
     /**
      * Swipes from a starting point to an ending point
      * @param startPoint: The vector representing the starting point of the swipe, this must be given in pixels (use {@code gestureController} to convert values)
      * @param endPoint: The vector representing the ending point of the swipe, this must be given in pixels (use {@code gestureController} to convert values)
      */
-    public void swipe(Vector startPoint, Vector endPoint) {
+    public void swipe(Vector startPoint, Vector endPoint, Duration duration) {
         PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
 
         Sequence sequence = new Sequence(finger, 0)
                 .addAction(finger.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), startPoint.getIntX(), startPoint.getIntY()))
                 .addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()))
-                .addAction(finger.createPointerMove(Duration.ofMillis(100), PointerInput.Origin.viewport(), endPoint.getIntX(), endPoint.getIntY()))
+                .addAction(finger.createPointerMove(duration, PointerInput.Origin.viewport(), endPoint.getIntX(), endPoint.getIntY()))
                 .addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
         driver.perform(List.of(sequence));
     }
